2cc3ddec936daa5fdc49c5f7c985cd12ba571a2e
Prabhu 543 <prabhuprathamesh444@gmail.com>
2025-02-23 16:16:05 +0530

    modified using 'useReducer' hook

--- React-Contact-Manager/src/App.jsx
+++ React-Contact-Manager/src/App.jsx
@@ -1,42 +1,22 @@
-import { useState } from "react";
+
 import Users from "./Components/Users" ;
 import AddContact from "./Components/AddContact" ;
+import UserDataContextProvider from "./store/UserData";
 
-// import { useContext } from "react";
-import { UserDataContext } from "./store/UserData";
 
 const App = () => {
-  const [userInfo , setUserInfo] = useState([]);
-
-  const addNewUser = (userName , userEmail) => {
-    // console.log(`${userName} at email is  ${userEmail}`);
-    let newUser = [...userInfo , {name : userName , email : userEmail}];
-    setUserInfo(newUser);
-  }
-
-  const deleteUser = ( email ) =>{
-    // console.log(`email : ${email}`);
-    const newUser = userInfo.filter((user) => user.email !== email);
-    setUserInfo(newUser);
-  }
-
   return (
-    <UserDataContext.Provider 
-      value={
-        {userInfo ,
-          addNewUser,
-          deleteUser
-        }}>
-    <div className="container">
+    <UserDataContextProvider>
+      <div className="container">
       <h1>Contact Manager</h1>
       <AddContact/>
       <hr />
       <div className="contactDiv">
         <h2>Contact List</h2>
-        <Users userInfo = {userInfo} userInfoLength = {userInfo.length} deleteBtn ={deleteUser}/>
+        <Users/>
       </div>
     </div>
-  </UserDataContext.Provider>
+    </UserDataContextProvider>
   )
 }
 
--- React-Contact-Manager/src/store/UserData.jsx
+++ React-Contact-Manager/src/store/UserData.jsx
@@ -1,7 +1,53 @@
-import { createContext  } from "react";
+import { createContext, useReducer  } from "react";
 
 export const UserDataContext = createContext({
   userInfo  : [],
   addNewUser : () => {},
   deleteUser : () => {},
 });
+
+
+const userInfoReducer = (currentUserInfo, action) => {
+  switch (action.type) {
+    case "ADD_USER":
+      return [...currentUserInfo, { name: action.payload.name, email: action.payload.email }];
+    case "DELETE_USER":
+      return currentUserInfo.filter((user) => user.email !== action.payload.email);
+    default:
+      return currentUserInfo; // Always return state if action is unknown
+  }
+};
+
+const UserDataContextProvider = ({ children }) => {
+  const [userInfo , dispatchUserInfo ] = useReducer(userInfoReducer , []);
+
+  const addNewUser = (userName , userEmail) => {
+    dispatchUserInfo({
+      type : "ADD_USER",
+      payload : {
+        name : userName, 
+        email : userEmail
+      }
+    });
+  }
+
+  const deleteUser = ( email ) =>{
+    dispatchUserInfo({
+      type : "DELETE_USER",
+      payload : {
+        email
+      }
+    });
+  }
+  return (
+    <UserDataContext.Provider 
+      value={
+        {userInfo ,
+          addNewUser,
+          deleteUser
+        }}>
+      {children}
+    </UserDataContext.Provider>
+  )
+}
+export default UserDataContextProvider;
